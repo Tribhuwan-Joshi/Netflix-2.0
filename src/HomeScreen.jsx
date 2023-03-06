abb4a319bf37c996ca262b19c850c4c8908af844
Tribhuwan <mathincosmos@gmail.com>
2023-03-06 12:27:02 +0530

    create home Component

---
+++ src/HomeScreen.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Nav from "./Nav"
+import "./HomeScreen.css"
+function HomeScreen() {
+  return (
+    <div className='homeScreen'>
+
+       <Nav/>
+    {/*  <Banner/>
+      <Row/> */}
+    </div>
+  );
+}
+
+export default HomeScreen;
