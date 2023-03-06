c045477b89fcdaa2e30e1d37073f46b64ce5413e
Tribhuwan <mathincosmos@gmail.com>
2023-03-06 12:27:30 +0530

    integrate firebase

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyASnkcGVWVOBCA9kxgSMRDvaQWk9QYzbqY",
+  authDomain: "netflix-a53c5.firebaseapp.com",
+  projectId: "netflix-a53c5",
+  storageBucket: "netflix-a53c5.appspot.com",
+  messagingSenderId: "1017879375497",
+  appId: "1:1017879375497:web:d7c367ceea0348d9df2058",
+};
