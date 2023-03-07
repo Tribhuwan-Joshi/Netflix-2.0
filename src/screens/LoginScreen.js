a6ef8cd8bbe1c95e3bc0489d5477c632a4839b88
Tribhuwan <mathincosmos@gmail.com>
2023-03-07 15:51:17 +0530

    design login page background

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -13,6 +13,25 @@ export default function LoginScreen() {
         <button className="signIn_btn">Sign In</button>
       </div>
       <div className="loginScreen_gradient" />
+      <div className="loginScreen_body">
+    <>
+    <h1>
+        Unlimited films , TV programmes and more.
+    </h1>
+    <h2>Watch anywhere. Cancel at any time.</h2>
+    <h3>Ready to watch? Enter your email to create or restart your memebership.</h3>
+
+    <div className="loginScreen_input">
+    <form>
+        <input type="email" placeholder="Email Address"/>
+        <button className="loginScreen_getStarted">GET STARTED</button>
+     
+    </form>
+
+    </div>
+    </>
+
+      </div>
     </div>
   );
 }
