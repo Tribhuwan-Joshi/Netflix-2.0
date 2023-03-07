4644347b1f678528dd87eecde3d0c048b5ec853f
Tribhuwan <mathincosmos@gmail.com>
2023-03-07 07:35:24 +0530

    populate banner data from api

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -1,31 +1,49 @@
-import React from 'react';
-import './Banner.css'
+import axios from "./axios";
+import React, { useEffect, useState } from "react";
+import "./Banner.css";
+import requests from "./Requests";
 
 function Banner() {
-  function trunctate(string ,n){
-    return string?.length > n ? string.substr(0,n-1) + "...":string
+  const [movie, setMovie] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetFlixOriginals);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+      return request;
+    }
+    fetchData();
+  }, []);
+  
+  function trunctate(string, n) {
+    return string?.length > n ? string.substr(0, n - 1) + "..." : string;
   }
   return (
-    <header className='banner' style={{
+    <header
+      className="banner"
+      style={{
         backgroundSize: "cover",
-        backgroundPosition:"center center",
-        backgroundImage:`url('https://upload.wikimedia.org/wikipedia/commons/c/cd/Black_flag.svg')`
-
-    }}>
-        <div className="banner_contents">
-            <div className="banner_title">Movie Name</div>
-            <div className="banner_buttons">
-
-                <button className="banner_button">Play</button>
-                <button className="banner_button">My List</button>
-            </div>
-            <h1 className='banner_description'>{trunctate(`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum`,200)}</h1>
+        backgroundPosition: "center center",
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+      }}
+    >
+      <div className="banner_contents">
+        <h1 className="banner_title">{movie?.title || movie?.name || movie?.original_name}</h1>
+        <div className="banner_buttons">
+          <button className="banner_button">Play</button>
+          <button className="banner_button">My List</button>
         </div>
-        <div className="banner-fadeBottom"/>
-
-       
-
-
+        <h1 className="banner_description">
+          {trunctate(
+         movie?.overview,
+            200
+          )}
+        </h1>
+      </div>
+      <div className="banner-fadeBottom" />
     </header>
   );
 }
