8152f0b434d394eaff08b6a0a14a7512084719bb
Tribhuwan <mathincosmos@gmail.com>
2023-03-07 21:26:09 +0530

    add sign In modal

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,36 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import "./LoginScreen.css";
 import logo from "../assets/netflixlogo.png";
 import loginPage from "../assets/loginPage.jpg";
+import SignUpScreen from "./SignUpScreen";
 export default function LoginScreen() {
+  const [signIn ,setSignIn] = useState(false);
   return (
     <div
       className="loginScreen"
-      style={{ background: `url(${loginPage}) no-repeat center ` , backgroundSize:'cover' }}
+      style={{
+        background: `url(${loginPage}) no-repeat center `,
+        backgroundSize: "cover",
+      }}
     >
       <div className="loginScreen_background">
         <img src={logo} className="loginScreen_logo" alt="logo" />
-        <button className="signIn_btn">Sign In</button>
+        <button className="signIn_btn" onClick={() => setSignIn(true)}>
+          Sign In
+        </button>
       </div>
       <div className="loginScreen_gradient" />
+    
       <div className="loginScreen_body">
-    <>
-    <h1>
-        Unlimited films , TV programmes and more.
-    </h1>
-    <h2>Watch anywhere. Cancel at any time.</h2>
-    <h3>Ready to watch? Enter your email to create or restart your memebership.</h3>
+        {signIn ? (
+          <SignUpScreen/>
+        ) : (
+          <>
+            <h1>Unlimited films , TV programmes and more.</h1>
+            <h2>Watch anywhere. Cancel at any time.</h2>
+            <h3>
+              Ready to watch? Enter your email to create or restart your
+              memebership.
+            </h3>
 
-    <div className="loginScreen_input">
-    <form>
-        <input type="email" placeholder="Email Address"/>
-        <button className="loginScreen_getStarted">GET STARTED</button>
-     
-    </form>
-
-    </div>
-    </>
-
+            <div className="loginScreen_input">
+              <form>
+                <input type="email" placeholder="Email Address" />
+                <button
+                  onClick={() => setSignIn(true)}
+                  className="loginScreen_getStarted"
+                >
+                  GET STARTED
+                </button>
+              </form>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
