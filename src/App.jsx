08467a1220f33871b8f1cbff077c91956679f55c
Tribhuwan <mathincosmos@gmail.com>
2023-03-08 09:03:24 +0530

    add feature to signIn user

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import HomeScreen from "./screens/HomeScreen";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginScreen from "./screens/LoginScreen";
+import { auth } from "./firebase";
+import { onAuthStateChanged } from "firebase/auth";
 function App() {
   const user = null;
+  useEffect(()=> {
+  const unsubscribe =   onAuthStateChanged(auth , (user)=>{
+      if(user){
+        console.log(user);
+      }
+      else{
+        // logout
+      }
+    })
+    return unsubscribe;
+  },[]);
   return (
     <div className="app">
       <Router>
