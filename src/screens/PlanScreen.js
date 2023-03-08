1d4b819cd3d10ecd80a9c7808918f837f21286d1
Tribhuwan <mathincosmos@gmail.com>
2023-03-08 20:13:26 +0530

    add state on plans

--- src/screens/PlanScreen.js
+++ src/screens/PlanScreen.js
@@ -1,21 +1,32 @@
 import React, { useState } from "react";
 import "./PlanScreen.css"
 function PlanScreen() {
-  const [products] = useState([
-    { name: "Premium", description: "4k + HDR" },
-    { name: "Basic", description: "720p" },
-    { name: "Standard", description: "1080p" },
+  const [products,setProducts] = useState([
+    { id :1 , name: "Premium", description: "4k + HDR" , isSubscribed:false },
+    {id:2, name: "Basic", description: "720p" , isSubscribed:false },
+    { id:3 , name: "Standard", description: "1080p" , isSubscribed:false },
   ]);
+  function handleClick(id){
+setProducts(products.map(p => {
+    if(p.id===id) return {...p ,isSubscribed:!p.isSubscribed };
+    return p;
+}))
+  }
   return (
     <div className="planScreen">
       {products.map((p) => {
         return (
-          <div key={p.name} className="planScreen_plan">
+          <div key={p.id} className="planScreen_plan">
             <div className="planScreen_info">
-              <h5>{p.name}</h5>
-              <h6>{p.description}</h6>
+              <h4>{p.name}</h4>
+              <h5>{p.description}</h5>
             </div>
-            <button>Subscribe</button>
+            <button
+              onClick={() => handleClick(p.id)}
+              style={{ backgroundColor: p.isSubscribed ? "gray" : "#e50914" }}
+            >
+              {p.isSubscribed ? "Unsubscribe" : "Subscribe"}
+            </button>
           </div>
         );
       })}
